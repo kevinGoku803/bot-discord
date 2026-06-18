13657f9bd7c7577df1f08b865d6f7311c6718683
kevinGoku803 <kevindiablito80@gmail.com>
2026-06-17 19:16:24 -0600

    bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,19 @@
-import discord
-import os
-from discord.ext import commands
 
-# Configuración del bot
+import discord
+from discord.ext import commands
+import os
+
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Bot conectado como {bot.user}')
+    print(f'¡Bot conectado como {bot.user}!')
 
 @bot.command()
 async def hola(ctx):
-    await ctx.send('¡Hola, Comandante! El sistema está operativo.')
+    await ctx.send('¡Hola, Comandante! Estoy a tus órdenes.')
 
-# Ejecución del bot usando una variable de entorno segura
-# (Esto buscará el token en la configuración secreta de GitHub)
 token = os.environ['DISCORD_TOKEN']
 bot.run(token)
